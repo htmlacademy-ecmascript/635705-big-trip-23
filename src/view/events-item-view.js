1e65ab08fee3d0f2cb72b46bf21e3c7205770c39
Oleh Parkhomenko <oleh.parkhom@gmail.com>
2024-05-06 21:49:08 -0600

    Adds presenter

---
+++ src/view/events-item-view.js
@@ -0,0 +1,25 @@
+import { createElement } from '../render.js';
+
+function createEventsItemTemplate() {
+  return `
+    <li class="trip-events__item"></li>
+  `;
+}
+
+export default class EventsItemView {
+  getTemplate() {
+    return createEventsItemTemplate();
+  }
+
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+
+    return this.element;
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+}
