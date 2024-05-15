41e0ecb90eb2e74f7a98989f4e2859586a74af3c
Oleh Parkhomenko <oleh.parkhom@gmail.com>
2024-05-15 01:55:10 -0600

    Introduces a data structure detailing the waypoint, along with any optional features and the final destination

---
+++ src/mock/destinations.js
@@ -0,0 +1,38 @@
+export const destinations = [
+  {
+    id: 'cfe416cq-10xa-ye10-8077-2fs9a01edcab',
+    description:
+      'Chamonix, is a beautiful city, a true asian pearl, with crowded streets.',
+    name: 'Chamonix',
+    pictures: [
+      {
+        src: 'http://picsum.photos/300/200?r=0.0762563005163317',
+        description: 'Chamonix parliament building',
+      },
+    ],
+  },
+  {
+    id: 'w8b93zfg-45mp-tz84-1234-abcde5678901',
+    description:
+      'Amsterdam, is a beautiful city, a true asian pearl, with crowded streets.',
+    name: 'Amsterdam',
+    pictures: [
+      {
+        src: 'http://picsum.photos/300/200?r=0.9261874632018752',
+        description: 'Amsterdam parliament building',
+      },
+    ],
+  },
+  {
+    id: 'x7c82hij-99qp-uq26-5678-uvwxyz9876543',
+    description:
+      'Geneva, is a beautiful city, a true asian pearl, with crowded streets.',
+    name: 'Geneva',
+    pictures: [
+      {
+        src: 'http://picsum.photos/300/200?r=0.5123948726382917',
+        description: 'Geneva parliament building',
+      },
+    ],
+  },
+];
