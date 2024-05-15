d6c90ed834070571236d787e03b8a40eb8abe6b9
Oleh Parkhomenko <oleh.parkhom@gmail.com>
2024-05-15 02:06:23 -0600

    Adjusts the view point and route edit forms to handle content from external sources and renders components based on the resulting 'fish' data

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 import TripFiltersView from './view/trip-filters-view.js';
 import TripSortView from './view/trip-sort-view.js';
 import BoardPresenter from './presenter/board-presenter.js';
+import PointModel from './model/point-model.js';
 import { render } from './render.js';
 
 const filters = document.querySelector('.trip-controls__filters');
 const tripEvents = document.querySelector('.trip-events');
-const boardPresenter = new BoardPresenter({ boardContainer: tripEvents });
+
+const pointModel = new PointModel();
+pointModel.init();
+
+const boardPresenter = new BoardPresenter({
+  boardContainer: tripEvents,
+  pointModel,
+});
 
 render(new TripFiltersView(), filters);
 render(new TripSortView(), tripEvents);
